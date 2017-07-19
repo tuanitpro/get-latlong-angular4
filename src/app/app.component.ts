9b04d45e75c1154bb10441372f9acbfade78031f
Lê Thanh Tuấn <tuanitpro@gmail.com>
2017-07-19 16:23:34 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,26 +38,7 @@ export class AppComponent {
 
   public model: any = {};
     public locationList = new Array();
-  public markers = [
-    {
-      lat: Number(51.673858),
-      lng: Number( 7.815982),
-      label: 'A',
-      draggable: true
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
+ 
   constructor(private http: Http) { }
 
   private getLatLongByLocation(name: String): Observable<any> {
--- src/app/marker.class.ts
+++
@@ -1,8 +0,0 @@
-
-export class marker {
-      public lat: number;
-      public lng: number;
-      public label?: string;
-      public draggable: boolean;
-}
-
